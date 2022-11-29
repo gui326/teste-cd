da913a4f176ef6f483c7df705791bb3724a3f060
gui326 <guilherme.326@hotmail.com>
2022-11-29 15:15:14 -0300

    Teste 222

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Teste cd</h1>
+      <h1>Teste cd 2</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
